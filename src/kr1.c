a9462c42df1993d728625184b6ceab628cdcab7e
Katya <aleinik.katya.04@gmail.com>
2025-02-24 10:56:32 +0300

    Добавлен файл README.md и программа на C

---
+++ src/kr1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    const char *text = "Контрольная работа 1";
+    const char *group = "Группа 9а";
+    const char *student_name = "Алейник Екатерина Евгеньевна";
+
+    printf("%s\n", text);
+    printf("Группа: %s\n", group);
+    printf("Студент: %s\n", student_name);
+
+    return 0;
+}
